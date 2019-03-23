797c787527e78d9000628e659abc885b42bca8ed
Steven Lee <lee.steven@wustl.edu>
2019-03-23 00:29:39 +0000

    practice login

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8" >
+</head>
+
+<body>
+Username: <input type="text" id="user" name="user"/>
+<input type="submit" value="Submit"/>
+</body>
+</html>
