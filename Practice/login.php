4c0528c91b04b9a37d3c581ab4a333f2f619d348
Steven Lee <lee.steven@wustl.edu>
2019-03-25 12:30:18 +0000

    Fixed minor bug with login

--- Practice/login.php
+++ Practice/login.php
@@ -6,7 +6,38 @@
 </head>
 
 <body>
-    <form action="results.php" method="POST">
+
+<?php
+    session_start();
+
+    if(isset($_POST['user'])){
+        //Open using file handler
+        $h= fopen("users.txt", "r");
+
+        //Boolean to see if username was found
+        $validator = FALSE;
+
+        //Reading File Line-by-Line
+        while(!feof($h)){
+            if($_POST['user'] == trim(fgets($h))){
+                $validator = TRUE;
+            }
+        }
+        //Checking if validator returned true or false to check if username was in txt file.
+        if($validator== TRUE){
+            echo "Success";
+            $_SESSION['login_user'] = $_POST['user'];
+            header("Location: buff-Success.php");
+        }
+        else{
+            header("Location: buff-Failure.php");
+        }
+        fclose($h);
+        exit();
+    }
+    ?>
+
+    <form method="POST">
         Username: <input type="text" id="user" name="user"/>
         <input type="submit" value="Submit"/>
     </form>
