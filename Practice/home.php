e399c6472fe628d418fe17b2463c1371d48d977c
Steven Lee <lee.steven@wustl.edu>
2019-03-30 01:26:52 +0000

    Added delete button

--- Practice/home.php
+++ Practice/home.php
@@ -31,14 +31,18 @@
     <form action="buff-logout.php" method="post">
         <input type="submit" value="Logout">
     </form>
-
+    </br>
     <?php
         $path   = sprintf("/opt/lampp/htdocs/uploads/%s", $username);
         $files = array_diff(scandir($path), array('.', '..'));
+
         foreach ($files as $file){
-            // echo $file . '<br>';
             $pathFile  = sprintf("uploads/%s/%s", $username, $file);
-            echo '<a href="' .$pathFile . '">' . $file . '</a>' . '<br>';
+            echo '<a href="' .$pathFile . '">' . trim($file) . '</a>';
+
+            echo ' <form  action="deleted.php" method="POST">';
+                echo '<input type="submit" name = "deleteFile" value="Delete" />';
+            echo '</form>';
         }
     ?>
 
